4ebff0d49c1e57ff8d533416d11f00fb177a34fe
Gaurav Dangwal <gdangwal268@gmail.com>
2024-04-15 15:57:49 +0530

    save changes

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,13 @@
+import styles from "./Buttons.module.css";
+
+const Buttons = ()=>{
+
+return <div className={styles.buttonsContainer}>
+<button className={styles.button}>C</button>
+<button className={styles.button}>C</button>
+<button className={styles.button}>C</button>
+</div>
+
+}
+
+export default Buttons;
